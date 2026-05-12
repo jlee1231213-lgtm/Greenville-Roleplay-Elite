4240b85043393e69df561396da1e05851395a5c5
Jayson Lee <jaysonlee@MacBookAir.fios-router.home>
2026-05-11 20:24:33 -0400

    Update welcome embed with beating hearts emoji, animated arrows, blue color #1a3e88, new image, and user avatar thumbnail

--- events/welcome-event.js
+++ events/welcome-event.js
@@ -2,9 +2,9 @@ const { EmbedBuilder } = require('discord.js');
 const Settings = require('../models/settings');
 
 const DEFAULT_WELCOME_EMBED = {
-  title: '## > <:Beige_flying_star:1500584694859694252> *__Greenville Roleplay Elite - Warm Welcomes!__*',
-  description: '<:dot:1500584469906591971> Welcome to **Greenville Roleplay Elite** {user}! To become a verified civilian, use `/verify` in <#1500626032959422694>! Ensure to check our <#1500663198293037076> channel to enter some great **giveaways!**\n\n> <:dasharrow:1500584579721990155> Furthermore, in need of assistance? Dont be scared to open a ticket in <#1500623825409278003>!\n-# Have a great stay!',
-  image: 'https://media.discordapp.net/attachments/1502790559486705755/1503083504353935621/Screenshot_20260509_094523_Discord.png?ex=6a020f19&is=6a00bd99&hm=e85c3454b61707ea9c93a4b892f233069dee61061f2c778297f71a1157a05744&=&format=webp&quality=lossless&width=2160&height=798',
+  title: '## > <a:beatinghearts:1503548942904983552>  *__Greenville Roleplay Elite - Warm Welcomes!__* <a:beatinghearts:1503548942904983552>',
+  description: '<a:animatedarrow:1503548961892601887>  Welcome to **Greenville Roleplay Elite** {user}! To become a verified civilian, use `/verify` in <#1500626032959422694>! Ensure to check our <#1500663198293037076> channel to enter some great **giveaways!**\n\n> <a:animatedarrow:1503548961892601887> Furthermore, in need of assistance? Don\'t be scared to open a ticket in <#1500623825409278003>!\n-# Have a great stay!',
+  image: 'https://media.discordapp.net/attachments/1492958669200031814/1503549418656370828/image.png?ex=6a03c104&is=6a026f84&hm=8b2a5fd5359d0047363bddda6a9095ab5c08806e377ec6fb83dec1b9388b5dc5&=&format=webp&quality=lossless&width=2334&height=654',
 };
 
 function applyWelcomeTokens(text, memberId) {
@@ -28,7 +28,7 @@ module.exports = {
     if (!channel) return;
 
     const embedData = settings.welcomeEmbed || DEFAULT_WELCOME_EMBED;
-    const color = '#368f4c';
+    const color = '#1a3e88';
 
     const title = applyWelcomeTokens(embedData.title || DEFAULT_WELCOME_EMBED.title, member.id);
     const description = applyWelcomeTokens(embedData.description || DEFAULT_WELCOME_EMBED.description, member.id);
@@ -36,7 +36,8 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setColor(color)
       .setTitle(title)
-      .setDescription(description);
+      .setDescription(description)
+      .setThumbnail(member.displayAvatarURL());
 
     if ((embedData.image || DEFAULT_WELCOME_EMBED.image)?.startsWith('http')) {
       embed.setImage(embedData.image || DEFAULT_WELCOME_EMBED.image);
