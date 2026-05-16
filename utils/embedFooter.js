7f421f8760f72ba6ebbb03b892e767fc91a48d5b
Jayson Lee <jaysonlee@MacBookAir.fios-router.home>
2026-05-16 13:34:57 -0400

    Use Greenville circle footer icon

--- utils/embedFooter.js
+++ utils/embedFooter.js
@@ -1,16 +1,10 @@
 const GREENVILLE_FOOTER_TEXT = 'Greenville Hub™';
+const GREENVILLE_FOOTER_ICON_URL = 'https://media.discordapp.net/attachments/1492958669200031814/1505251172150411466/kiaodogcircle_2_.png?ex=6a09f1e5&is=6a08a065&hm=cc710655fb31f9ddd95ec63a37b5b7d48d47ca0308917ecbf724b6415cc3b95d&=&format=webp&quality=lossless&width=818&height=818';
 
-function resolveGuild(source) {
-  return source?.guild || source;
-}
-
-function greenvilleFooter(source) {
-  const guild = resolveGuild(source);
-  const iconURL = guild?.iconURL?.({ extension: 'png', size: 128 }) || undefined;
-
+function greenvilleFooter() {
   return {
     text: GREENVILLE_FOOTER_TEXT,
-    iconURL,
+    iconURL: GREENVILLE_FOOTER_ICON_URL,
   };
 }
 
@@ -20,6 +14,7 @@ function applyGreenvilleFooter(embed, source) {
 
 module.exports = {
   GREENVILLE_FOOTER_TEXT,
+  GREENVILLE_FOOTER_ICON_URL,
   greenvilleFooter,
   applyGreenvilleFooter,
 };
