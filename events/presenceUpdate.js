8e29f2a8d7b13ee714c0c17ec1754f9a944f9e21
Jayson Lee <jaysonlee@MacBookAir.fios-router.home>
2026-05-16 13:06:03 -0400

    Add Greenville Hub footer and logo to Server Supporters embed (presenceUpdate)

--- events/presenceUpdate.js
+++ events/presenceUpdate.js
@@ -40,6 +40,7 @@ module.exports = {
 <a:GVH_animatedarrow:1504244827062010131> Thank you for supporting **__Greenville Hub__** ${user} You will **__recieve__** the <@&1505209789741797436> role for putting /gvhub in you status!
 
 > <a:GVH_animatedarrow:1504244827062010131> Want the <@&1505209789741797436> role...? Ensure to put /gvhub in your status & you will receive it!`);
+            .setFooter({ text: 'Greenville Hub™', iconURL: 'https://media.discordapp.net/attachments/1492958669200031814/1505251172150411466/kiaodogcircle_2_.png?ex=6a09f1e5&is=6a08a065&hm=cc710655fb31f9ddd95ec63a37b5b7d48d47ca0308917ecbf724b6415cc3b95d&=&format=webp&quality=lossless&width=818&height=818' });
 
         await channel.send({ embeds: [embed] });
       } catch (error) {
