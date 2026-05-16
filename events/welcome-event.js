4fcb1f104a717747e90ab0189f3e8a00b84dcfb7
Jayson Lee <jaysonlee@MacBookAir.fios-router.home>
2026-05-16 14:10:28 -0400

    Update welcome embed template

--- events/welcome-event.js
+++ events/welcome-event.js
@@ -4,9 +4,9 @@ const Settings = require('../models/settings');
 const DEFAULT_WELCOME_CHANNEL_ID = '1500619986295722014';
 
 const DEFAULT_WELCOME_EMBED = {
-  title: '## > <a:beatinghearts:1503873058073481316>   *__Greenville Hub - Warm Welcomes!__* <a:beatinghearts:1503873058073481316>',
-  description: '<a:animatedarrow:1503873075010211943>   Welcome to **Greenville Hub** {user}! To become a verified civilian, use `/verify` in <#1500626032959422694>! Ensure to check our <#1500663198293037076> channel to enter some great **giveaways!**\n\n> <a:animatedarrow:1503873075010211943>  Furthermore, in need of assistance? Don\'t be scared to open a ticket in <#1500623825409278003>!\n-# Have a great stay!',
-  image: 'https://media.discordapp.net/attachments/1492895569927077921/1502667820977094837/Screenshot_20260509_094523_Discord.jpg?ex=6a048076&is=6a032ef6&hm=e80b0725b0957423c3d5740a2c780e53af2d0e7958a3fdd49bb75008d775598e&=&format=webp&width=2160&height=798',
+  title: '# > <a:GVH_beatinghearts:1504244806803783717> *Greenville Hub - __Warm Welcomes__* <a:GVH_beatinghearts:1504244806803783717>',
+  description: '<a:GVH_animatedarrow:1504244827062010131> Welcome to **__Greenville Hub™__** {user}! Please make sure you are verified by running the `/verify` command in <#1500626032959422694> using "Dyno". Please ensure you have read over our informative, found in <#1500620852633272350>!\n\n> <a:GVH_animatedarrow:1504244827062010131> Furthermore, requiring assistance? Simple, open a ticket in <#1500623825409278003>!',
+  image: 'https://media.discordapp.net/attachments/1492958669200031814/1505270658794258552/image.png?ex=6a0a040b&is=6a08b28b&hm=d736c9464ba8b938aa4a1ddaa3183d706a943ad9d56bf37f083d7ab8c9aaee32&=&format=webp&quality=lossless&width=2352&height=728',
 };
 
 const LEGACY_WELCOME_EMBED = {
@@ -15,22 +15,41 @@ const LEGACY_WELCOME_EMBED = {
   image: 'https://media.discordapp.net/attachments/1492958669200031814/1503549418656370828/image.png?ex=6a03c104&is=6a026f84&hm=8b2a5fd5359d0047363bddda6a9095ab5c08806e377ec6fb83dec1b9388b5dc5&=&format=webp&quality=lossless&width=2334&height=654',
 };
 
+const LEGACY_WELCOME_TITLE_MARKERS = [
+  '<a:beatinghearts:1503873058073481316>',
+  '<a:beatinghearts:1503548942904983552>',
+];
+
+const LEGACY_WELCOME_DESCRIPTION_MARKERS = [
+  '<a:animatedarrow:1503873075010211943>',
+  '<a:animatedarrow:1503548961892601887>',
+];
+
+const LEGACY_WELCOME_IMAGES = [
+  'https://media.discordapp.net/attachments/1492895569927077921/1502667820977094837/Screenshot_20260509_094523_Discord.jpg?ex=6a048076&is=6a032ef6&hm=e80b0725b0957423c3d5740a2c780e53af2d0e7958a3fdd49bb75008d775598e&=&format=webp&width=2160&height=798',
+  LEGACY_WELCOME_EMBED.image,
+];
+
 function normalizeBranding(text) {
   if (!text) return text;
   return text.replace(/Greenville Roleplaye? Elite/gi, 'Greenville Hub');
+}
+
+function isLegacyWelcomeValue(value, legacyValue, markers = []) {
+  return !value || value === legacyValue || markers.some(marker => value.includes(marker));
 }
 
 function resolveWelcomeEmbed(savedEmbed) {
   if (!savedEmbed) return DEFAULT_WELCOME_EMBED;
 
   return {
-    title: !savedEmbed.title || savedEmbed.title === LEGACY_WELCOME_EMBED.title
+    title: isLegacyWelcomeValue(savedEmbed.title, LEGACY_WELCOME_EMBED.title, LEGACY_WELCOME_TITLE_MARKERS)
       ? DEFAULT_WELCOME_EMBED.title
       : savedEmbed.title,
-    description: !savedEmbed.description || savedEmbed.description === LEGACY_WELCOME_EMBED.description
+    description: isLegacyWelcomeValue(savedEmbed.description, LEGACY_WELCOME_EMBED.description, LEGACY_WELCOME_DESCRIPTION_MARKERS)
       ? DEFAULT_WELCOME_EMBED.description
       : savedEmbed.description,
-    image: !savedEmbed.image || savedEmbed.image === LEGACY_WELCOME_EMBED.image
+    image: !savedEmbed.image || LEGACY_WELCOME_IMAGES.includes(savedEmbed.image)
       ? DEFAULT_WELCOME_EMBED.image
       : savedEmbed.image,
   };
@@ -42,6 +61,16 @@ function applyWelcomeTokens(text, memberId) {
     .replace(/\{user\}|\$user/g, `<@${memberId}>`)
     .replace(/\$date/g, new Date().toLocaleDateString())
     .replace(/\\n/g, '\n');
+}
+
+function applyWelcomeContent(embed, title, description) {
+  if (title?.trim().startsWith('#')) {
+    return embed.setDescription(`${title}\n${description || ''}`);
+  }
+
+  if (title) embed.setTitle(title);
+  if (description) embed.setDescription(description);
+  return embed;
 }
 
 module.exports = {
@@ -64,9 +93,8 @@ module.exports = {
 
     const embed = new EmbedBuilder()
       .setColor(color)
-      .setTitle(title)
-      .setDescription(description)
       .setThumbnail(member.displayAvatarURL());
+    applyWelcomeContent(embed, title, description);
 
     if ((embedData.image || DEFAULT_WELCOME_EMBED.image)?.startsWith('http')) {
       embed.setImage(embedData.image || DEFAULT_WELCOME_EMBED.image);
